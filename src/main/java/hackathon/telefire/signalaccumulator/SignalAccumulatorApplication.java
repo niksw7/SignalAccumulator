3ea3ec3f7cda6cf149c4441883c54559b392bd45
niksw7 <nikeshs@thoughtworks.com>
2017-09-30 12:28:35 +0530

    Setting up app

---
+++ src/main/java/hackathon/telefire/signalaccumulator/SignalAccumulatorApplication.java
@@ -0,0 +1,12 @@
+package hackathon.telefire.signalaccumulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignalAccumulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignalAccumulatorApplication.class, args);
+	}
+}
